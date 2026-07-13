2dee8930664e5680054f44378f2bf903c6328998
pratikshivale <pratikshivale07@gmail.com>
2026-07-13 11:04:49 +0530

    hello print question

---
+++ basic/q1.java
@@ -0,0 +1,16 @@
+package basic;
+import java.util.*;
+
+
+public class q1 {
+    public static void main(String[]args)
+    {
+        
+        System.out.println("Hello World");
+
+    }
+    
+}
+
+
+
