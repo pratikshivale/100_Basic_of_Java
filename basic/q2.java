7d0924ef7217b29eb014b5def0a7eb2f15c0323d
pratikshivale <pratikshivale07@gmail.com>
2026-07-13 11:24:50 +0530

    sum of 2 number

---
+++ basic/q2.java
@@ -0,0 +1,19 @@
+package basic;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class q2 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number a:");
+        int a = sc.nextInt();
+        System.out.println("Enter the number b:" );
+        int b = sc.nextInt();
+
+        int sum = a+b;
+        System.out.println(sum);
+
+    }
+    
+}
