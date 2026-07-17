764f954c7ca872089707213189668e3541350a52
pratikshivale <pratikshivale07@gmail.com>
2026-07-17 10:37:16 +0530

    These code for calculate the area and circumference of circle

---
+++ basic/q4.java
@@ -0,0 +1,26 @@
+package basic;
+import java.util.*;
+
+public class q4 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the circle radius: " );
+        double radius = sc.nextDouble();
+        double pi = 3.14;
+        System.out.println("The area of circle is : " + (pi*(radius*radius)));
+        System.out.println("The circumference of circle is : "+ (2*(pi*radius)));
+
+
+        
+
+
+
+
+
+
+
+
+    }
+    
+}
