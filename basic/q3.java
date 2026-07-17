baad531130ae57343a56abd991e37aff2b3dbe26
pratikshivale <pratikshivale07@gmail.com>
2026-07-17 10:14:15 +0530

    These code of calculating the two numbers

---
+++ basic/q3.java
@@ -0,0 +1,45 @@
+package basic;
+import java.util.*;
+
+
+public class q3 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number a: ");
+        int a= sc.nextInt();
+
+        System.out.print("Enter number b: ");
+        int b =sc.nextInt();
+
+        System.out.println("1.Addtion "+ (a+b));
+        System.out.println("2.Subtraction "+ (a-b));
+        System.out.println("3.Multipication "+ (a*b));
+        System.out.println("4.Division "+(a/b));
+
+        // System.out.println("Choose The operation no");
+        // int operation = sc.nextInt();
+        // int sum;
+
+        // if(operation == 1){
+        //      sum = a+b;
+        //     System.out.print(sum);
+        // }
+        // else if(operation == 2){
+        //     sum = a-b;
+        //     System.out.print(sum);
+        // }
+        // else if(operation == 3){
+        //     sum = a*b;
+        //     System.out.print(sum);
+        // }
+        // else if(operation == 4){
+        //     sum = a%b;
+        //     System.out.print(sum);
+        // }
+        // else{
+        //     System.out.println("Invalid option is selected");
+        // }
+
+    }
+    
+}
